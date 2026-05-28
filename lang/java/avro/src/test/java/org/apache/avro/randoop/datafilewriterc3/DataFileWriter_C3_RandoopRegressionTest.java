9510bf5a6661c8b51aea1c3032cd9825f54e44b8
AlessandroLori <alessandrolori179@gmail.com>
2026-05-28 14:24:56 +0200

    Trash Framework Randoop ClassesCn and Fixes

---
+++ lang/java/avro/src/test/java/org/apache/avro/randoop/datafilewriterc3/DataFileWriter_C3_RandoopRegressionTest.java
@@ -0,0 +1,10 @@
+package org.apache.avro.randoop.datafilewriterc3;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ DataFileWriter_C3_RandoopRegressionTest0.class, DataFileWriter_C3_RandoopRegressionTest1.class, DataFileWriter_C3_RandoopRegressionTest2.class })
+public class DataFileWriter_C3_RandoopRegressionTest {
+}
+
---
+++ lang/java/avro/src/test/java/org/apache/avro/randoop/datafilewriterc4/DataFileWriter_C4_RandoopRegressionTest.java
@@ -0,0 +1,10 @@
+package org.apache.avro.randoop.datafilewriterc4;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ DataFileWriter_C4_RandoopRegressionTest0.class, DataFileWriter_C4_RandoopRegressionTest1.class, DataFileWriter_C4_RandoopRegressionTest2.class })
+public class DataFileWriter_C4_RandoopRegressionTest {
+}
+
